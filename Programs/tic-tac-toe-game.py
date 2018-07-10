bc0025a78bd78c741e6cb03baac6a4890d9077e4
Tushar <viztushar@gmail.com>
2018-07-10 23:53:18 +0530

    bug fix

--- Programs/tic-tac-toe-game.py
+++ Programs/tic-tac-toe-game.py
@@ -61,46 +61,56 @@ def is_game_over(board):
                 col_win(board, player) or
                 diag_win(board, player)):
             winner = player
-            print("Winner is:" + str(winner))
+            print("Player " + ((str)(winner)) + " is winner")
 
     if np.all(board != 0) and winner == 0:
         winner = -1
+
+    if (winner == -1):
+        print("draw")
     return winner
 
 
 
 def input_board(loc, board, player):
-    if (loc == 1):
+    if (loc == 1) and not((board[0, 0] == 1) or (board[0, 0] == 2)):
         board[0, 0] = player
-    elif (loc == 2):
+    elif (loc == 2) and not((board[0, 1] == 1) or (board[0, 1] == 2)):
         board[0, 1] = player
-    elif (loc == 3):
+    elif (loc == 3) and not((board[0, 2] == 1) or (board[0, 2] == 2)):
         board[0, 2] = player
-    elif (loc == 4):
+    elif (loc == 4) and not((board[1, 0] == 1) or (board[1, 0] == 2)):
         board[1, 0] = player
-    elif (loc == 5):
+    elif (loc == 5) and not((board[1, 1] == 1) or (board[1, 1] == 2)):
         board[1, 1] = player
-    elif (loc == 6):
+    elif (loc == 6) and not((board[1, 2] == 1) or (board[1, 2] == 2)):
         board[1, 2] = player
-    elif (loc == 7):
+    elif (loc == 7) and not((board[2, 0] == 1) or (board[2, 0] == 2)):
         board[2, 0] = player
-    elif (loc == 8):
+    elif (loc == 8) and not((board[2, 1] == 1) or (board[2, 1] == 2)):
         board[2, 1] = player
-    elif (loc == 9):
+    elif (loc == 9) and not((board[2, 2] == 1) or (board[2, 2] == 2)):
         board[2, 2] = player
+
+    else:
+        print("you cant choose this place")
+        print("Player " + ((str)(player)) +" choose where to place")
+        n = int(input(">> "))
+        board = input_board(n,board,player)
     return (board)
+
 
 
 def play_game():
     c,winner = 0,0
     board = create_board()
+    print(board)
     while winner == 0:
-        print(board)
         print("Player 1 choose where to place")
         n = int(input(">> "))
         board = input_board(n, board, 1)
         print(board)
-        if (is_game_over(board)):
+        if(is_game_over(board)):
             break
         print("Player 2 choose where to place")
         n = int(input(">> "))
